4cf7955635c867c728493eccbd396661c84bcba4
MarcinhoFIVE <darlan_lifecat@yahoo.com.br>
2022-04-29 18:58:23 -0300

    Project Permissions

--- app/src/main/java/br/mdan/localizationproject/MapsActivity.kt
+++ app/src/main/java/br/mdan/localizationproject/MapsActivity.kt
@@ -1,25 +1,39 @@
 package br.mdan.localizationproject
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import br.mdan.localizationproject.databinding.ActivityMapsBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
-import br.mdan.localizationproject.databinding.ActivityMapsBinding
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.tasks.OnCompleteListener
+
 
 class MapsActivity : AppCompatActivity(),
-    OnMapReadyCallback,
-GoogleMap.OnMarkerClickListener {
+                        OnMapReadyCallback,
+                            GoogleMap.OnMarkerClickListener {
 
     private lateinit var map: GoogleMap
     private lateinit var binding: ActivityMapsBinding
     private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var lastLocation: Location
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
+        private const val COARSE_PERMISSION_REQUEST_CODE = 2
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,12 +62,44 @@ GoogleMap.OnMarkerClickListener {
         map = googleMap
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        map.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        //val sydney = LatLng(-34.0, 151.0)
+//        val myPlace = LatLng(40.73, -73.99)
+//        map.addMarker(MarkerOptions().position(myPlace).title("My Favourite City"))
+//        map.moveCamera(CameraUpdateFactory.newLatLngZoom(myPlace, 12.0f))
 
         map.getUiSettings().setZoomControlsEnabled(true)
         map.setOnMarkerClickListener(this)
+
+        setUpMap()
+    }
+
+    //@SuppressLint("MissingPermission")
+    private fun setUpMap() {
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) !=
+            PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION) !=
+            PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    android.Manifest.permission.ACCESS_COARSE_LOCATION),
+                LOCATION_PERMISSION_REQUEST_CODE)
+
+            return
+        }
+
+        map.isMyLocationEnabled = true
+
+        fusedLocationClient.lastLocation.addOnSuccessListener(this) { location ->
+            if (location != null) {
+                lastLocation = location
+                val currentLatLng = LatLng(location.latitude, location.longitude)
+                map.addMarker(MarkerOptions().title("My Location"))
+                map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng,12f))
+            }
+        }
     }
 
     override fun onMarkerClick(p0: Marker) = false
