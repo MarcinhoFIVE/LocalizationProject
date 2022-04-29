49b357f76f400f4b39c2f041c1d8b8b24f796fea
MarcinhoFIVE <darlan_lifecat@yahoo.com.br>
2022-04-29 14:28:48 -0300

    Map Showing

--- app/src/main/java/br/mdan/localizationproject/MapsActivity.kt
+++ app/src/main/java/br/mdan/localizationproject/MapsActivity.kt
@@ -2,7 +2,6 @@ package br.mdan.localizationproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
